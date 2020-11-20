7e9b6e561b56e3f67a145e7b16f7c2909376ccb3
EliasSchramm <privat@eps-dev.de>
2020-11-20 21:03:47 +0100

    Content | Style

--- src/content.php
+++ src/content.php
@@ -64,7 +64,7 @@
       <label for="f2_t2">Key: </label>
       <input type="number" id="f2_i1" name="f2_i1" value="1">
 
-      <button type="submit">ENTSCHLÜSSELN"</button>
+      <button type="submit">ENTSCHLÜSSELN</button>
     </form>
 
     <p>Output:  <span id = "out2"></span> </p>
@@ -73,12 +73,47 @@
   </section>
 
   <section class="content_section">
-    <h1>Seite 4</h1>
+    <h1>Etwas Sicherer</h1>
+    <h2 class="animated" animated="NUN;ZÄHLEN;WIR" sec="120" index="0" subindex="0" hold="500" reverse="50">ss</h2>
+    <hr>
+    <p>
+      Ein Grundstein liegt nun.
+      Im nächsten Beispiel möchte ich mich darum kümmern dass man problemlos häufige Zeichen wie Leerzeichen oder 'e'
+      aus dem verschlüsselten Text heraussuchen kann und damit den Text problemlos entschlüsseln kann. <br>
+      Wie macht man das? <br>
+      Nun ja, hier gibt es wieder unendlich viele Möglichkeiten. Ich möchte allerdings die wohl simpelste Variante wählen: Das Erhöhen des Key für jedes Zeichen. <br>
+      So wird z.B. bei der Zeichenkette 'Moin' mit Key(1) das 'M' mit Key(1) verschlüsselt, das 'o' mit Key(2), 'i' mit Key(3) usw.
+    </p>
     <p class="menu"> <span onclick="switchPage(-1)">-ZURÜCK-</span> | <span onclick="switchPage(+1)">-WEITER-</span> </p>
   </section>
 
   <section class="content_section">
-    <h1>Seite 5</h1>
+    <h1>Etwas Sicherer</h1>
+    <h2 class="animated" animated="NUN;ZÄHLEN;WIR" sec="120" index="0" subindex="0" hold="500" reverse="50">ss</h2>
+    <hr>
+    <form id="form1_bsp2" action="" method="post" >
+      <label for="f3_t1">Text: </label>
+      <textarea cols="40" rows="4" type="text" name="f3_t1" id="f3_t1" value="Text"></textarea>
+
+      <label for="f3_t2">Key: </label>
+      <input type="number" name="f3_i1" id="f3_i1" value="1">
+
+      <button type="submit">VERSCHLÜSSELN</button>
+    </form>
+
+    <p>Output:  <span id = "out3"></span> </p>
+
+    <form id="form2_bsp2" action="#" method="post">
+      <label for="f4_t1">Text: </label>
+      <textarea cols="40" rows="4" id="f4_t1" name="f4_t1" value="Text"> </textarea>
+
+      <label for="f4_t2">Key: </label>
+      <input type="number" id="f4_i1" name="f4_i1" value="1">
+
+      <button type="submit">ENTSCHLÜSSELN</button>
+    </form>
+
+    <p>Output:  <span id = "out4"></span> </p>
     <p class="menu"> <span onclick="switchPage(-1)">-ZURÜCK-</span> | <span onclick="switchPage(+1)">-WEITER-</span> </p>
   </section>
 
